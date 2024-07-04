8947adc620dad3fddb7f7377d975f3359dd9f48d
Sohan <sosohan404@gmail.com>
2024-07-04 16:35:45 +0545

    Added "New Questions Will Be Added Accordingly, Keep Checking The App :)" text to four subject pages: InstrumentationII, InstrumentationI, Internet and Intranet, and Irrigation and Drainage Engineering.

--- lib/Subject_Pages/InstrimentationII.dart
+++ lib/Subject_Pages/InstrimentationII.dart
@@ -467,6 +467,15 @@ class InstrumentationII extends StatelessWidget {
                     'https://notesioe.com/wp-content/uploads/2024/05/Instrumentation-II.pdf');
               },
             ),
+            SizedBox(height: 30),
+            Text(
+              "New Questions Will Be Added Accordingly,\nKeep Checking The App :)",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic),
+            )
           ],
         ),
       ),
