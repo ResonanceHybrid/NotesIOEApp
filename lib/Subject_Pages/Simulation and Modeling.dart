9d1e897227671139f554accb2dc9f75bacf56329
Sohan <sosohan404@gmail.com>
2024-07-04 16:19:13 +0545

    questions

--- lib/Subject_Pages/Simulation and Modeling.dart
+++ lib/Subject_Pages/Simulation and Modeling.dart
@@ -368,12 +368,22 @@ class SimulationandModeling extends StatelessWidget {
             SizedBox(height: 20),
             InsideButtons(
               text: 'Old Questions',
+              fileSize: '2',
               icon: Icons.question_answer_outlined,
               onTap: () {
                 openPDF(context,
-                    'https://notesioe.com/wp-content/uploads/2024/04/AppliedMechanicsOldQues.pdf');
+                    'https://notesioe.com/wp-content/uploads/2023/04/Simulation-and-Modeling.pdf');
               },
             ),
+            SizedBox(height: 30),
+            Text(
+              "New Questions Will Be Added Accordingly,\nKeep Checking The App :)",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic),
+            )
           ],
         ),
       ),
